2d6fb381e076491e94ec2fc4351f7b98195bd1f9
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2020-06-08 20:30:13 +0300

    Added tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hanzki/hello
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.1
--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/hanzki/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
